6d377fa8966ede66b2c5dc0da2b99a8b966be4f1
Thomas <thomas.cousiney@gmail.com>
2025-04-01 16:51:38 +0200

    Modification du fichier ssh

--- src/script.sh
+++ src/script.sh
@@ -1,8 +1,18 @@
 if [[ -d "./src" && -d "./site" ]]; then
     echo "Tout est ok."
-    exit 0
-
 else
     echo "Le fichier n'existe pas."
+    exit 1
+fi
+
+# Vérification des fichiers dans le dossier "site"
+invalid_files=$(find ./site -type f ! -name "*.img" ! -name "*.css" ! -name "*.js")
+
+if [[ -z "$invalid_files" ]]; then
+    echo "Le dossier 'site' contient uniquement des fichiers img, css et js."
+    exit 0
+else
+    echo "Erreur : Le dossier 'site' contient des fichiers non autorisés :"
+    echo "$invalid_files"
     exit 1
 fi
